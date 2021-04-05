2f6123f3e48264b930775bfb9517cc865dcc33f6
Titus Wormer <tituswormer@gmail.com>
2021-04-05 18:07:39 +0200

    Update dev-dependencies

--- test.js
+++ test.js
@@ -3,7 +3,7 @@
 var test = require('tape')
 var weasels = require('.')
 
-test('weasels', function(t) {
+test('weasels', function (t) {
   t.equal(typeof weasels, 'object', 'should be an array #1')
   t.equal(Array.isArray(weasels), true, 'should be an array #2')
   t.notEqual(weasels.indexOf('several'), -1, 'should contain words')
