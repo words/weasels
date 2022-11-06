a60595cf04009a9086b5f48c381dbd4b6b7c45f9
Titus Wormer <tituswormer@gmail.com>
2022-11-06 13:25:08 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,10 +1,9 @@
-import test from 'tape'
+import assert from 'node:assert'
+import test from 'node:test'
 import {weasels} from './index.js'
 
-test('weasels', function (t) {
-  t.equal(typeof weasels, 'object', 'should be an array #1')
-  t.equal(Array.isArray(weasels), true, 'should be an array #2')
-  t.notEqual(weasels.indexOf('several'), -1, 'should contain words')
-
-  t.end()
+test('weasels', function () {
+  assert.equal(typeof weasels, 'object', 'should be an array #1')
+  assert.equal(Array.isArray(weasels), true, 'should be an array #2')
+  assert.notEqual(weasels.indexOf('several'), -1, 'should contain words')
 })
