eba7415f9d157c8b77a20f97845d8671e4e6ea29
Titus Wormer <tituswormer@gmail.com>
2022-11-06 13:23:29 +0100

    Refactor code-style

--- build.js
+++ build.js
@@ -1,9 +1,9 @@
 import fs from 'node:fs'
 import {toJson} from 'plain-text-data-to-json'
 
-var doc = fs.readFileSync('data.txt', 'utf8')
+const doc = fs.readFileSync('data.txt', 'utf8')
 
 fs.writeFileSync(
   'index.js',
-  'export var weasels =' + JSON.stringify(toJson(doc), null, 2) + '\n'
+  'export const weasels =' + JSON.stringify(toJson(doc), null, 2) + '\n'
 )
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export var weasels = [
+export const weasels = [
   'a lot',
   'about',
   'acts',
